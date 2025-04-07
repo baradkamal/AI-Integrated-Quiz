e634e378f6fe36c197527b218dd5a09e7a88054f
baradkamal <baradk999@gmail.com>
2025-04-07 17:00:19 +0530

    in this after login route setting change like according to user redirect them to dashboard or home page

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,20 +1,25 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
+
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const platformId = inject(PLATFORM_ID);
   
-  
   if (isPlatformBrowser(platformId)) {
-    const loggedData = localStorage.getItem("authToken");
-    if (loggedData !== null) {
+    const token = localStorage.getItem("authToken");
+    const isAdmin = localStorage.getItem("admin") === 'true';
+    
+    if (token) {
+      // Redirect based on admin status if trying to access root
+      if (state.url === '/') {
+        router.navigateByUrl(isAdmin ? '/deshboard' : '/home');
+        return false;
+      }
       return true;
     }
   }
-  
   
   router.navigateByUrl("login");
   return false;
